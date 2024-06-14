b6d9dc5406f340617e289debceecb36803aae1d8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-14 14:18:36 +0100

    Updated testing in App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,16 @@ it("should have Community Hub", async () => {
     const message = await screen.findByText(/Community Hub/i);
     expect(message).toBeVisible();
 })
+
+it("should have Header.tsx, Main.tsx and Footer.tsx components", async () => {
+    render(<App />);
+
+    const headerComponent = await screen.findByRole('header');
+    expect(headerComponent).toBeVisible();
+
+    const mainComponent = await screen.findByRole('main');
+    expect(mainComponent).toBeVisible();
+
+    const footerComponent = await screen.findByRole('footer');
+    expect(footerComponent).toBeVisible();
+})
