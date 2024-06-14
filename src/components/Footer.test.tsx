89d1a73e9a954742a0b2149878cb1124a3f2ab12
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-14 18:26:51 +0100

    Updated tests in Footer.test.tsx

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -9,13 +9,25 @@ describe("Header component", () => {
 
   it("renders portfolio link correctly", () => {
     render(<Footer />);
+
+    const portfolioLink = screen.findAllByText(/Luna Smith/i);
+    expect(portfolioLink).toBeVisible();
   });
 
   it("renders GitHub link correctly", () => {
     render(<Footer />);
+
+    const GitHubLink = screen.findAllByText(/GitHub/i);
+    expect(GitHubLink).toBeVisible();
   });
 
   it("renders text correctly", () => {
     render(<Footer />);
+
+    const texElement1 = screen.findAllByText(/Coded by/i);
+    expect(texElement1).toBeVisible();
+
+    const texElement2 = screen.findAllByText(/open-source/i);
+    expect(texElement2).toBeVisible();
   });
 });
