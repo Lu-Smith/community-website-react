e0824d8954a3c2fa0ad65068d4ae26e6c85eff8b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-14 13:51:04 +0100

    Updated App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import App from "./App";
 
-it("should have Hello Worlds", async () => {
+it("should have Community Hub", async () => {
     render(<App />);
     const message = await screen.findByText(/Community Hub/i);
     expect(message).toBeVisible();
