c8f44d1eae4362a11d311971d0238cdcc36d167e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-14 18:23:53 +0100

    Created Footer.test.tsx

---
+++ src/components/Footer.test.tsx
@@ -0,0 +1,21 @@
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom';
+import Footer from "./Footer";
+
+describe("Header component", () => {
+  it("renders without crashing", () => {
+    render(<Footer />);
+  });
+
+  it("renders portfolio link correctly", () => {
+    render(<Footer />);
+  });
+
+  it("renders GitHub link correctly", () => {
+    render(<Footer />);
+  });
+
+  it("renders text correctly", () => {
+    render(<Footer />);
+  });
+});
