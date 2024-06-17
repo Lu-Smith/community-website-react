bd3b4e6161e047ba023b569e92ea5634e718609d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-17 20:49:58 +0100

    Created FooterHelpers.tsx

--- src/components/FooterHelpers.tsx
+++ src/components/FooterHelpers.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const HelpersWrapper = styled.section`
+const HelpersWrapper = styled.div`
   padding: 4em;
   background: white;
   color: purple;
 `;
 
+const ContactContainer = styled.ul`
+  padding: 4em;
+  background: white;
+  color: purple;
+`;
+
+const ContactLi = styled.li`
+  background: yellow;
+  color: purple;
+`;
+
+const HelpersContainer = styled.ul`
+  padding: 4em;
+  background: white;
+  color: purple;
+`;
+
+const ListLi = styled.li`
+  background: yellow;
+  color: purple;
+`;
+
+
 const FooterHelpers: React.FC = () => {
   return (
-    <HelpersWrapper>FooterHelpers</HelpersWrapper>
+    <HelpersWrapper>
+      <ContactContainer data-testid='contactContainer'>
+        <ContactLi role='contactLink'>Contact us</ContactLi>
+        <ContactLi data-testid='phoneNumber'>+1-543-123-4567</ContactLi>
+        <ContactLi data-testid='email'>example@bubble.com</ContactLi>
+      </ContactContainer>
+      <HelpersContainer data-testid='helpersContainer'>
+        <ListLi role='homeLink'>About Us</ListLi>
+        <ListLi role='faqLink'>FAQ</ListLi>
+        <ListLi role='careerLink'>Carrer</ListLi>
+        <ListLi role='blogLink'>Blog</ListLi>
+        <ListLi role='supportLink'>Support</ListLi>
+      </HelpersContainer>
+    </HelpersWrapper>
   )
 }
 
