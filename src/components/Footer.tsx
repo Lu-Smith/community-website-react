8ee3cd7d3386679f038d9f00d736e77e6108b306
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-21 21:08:45 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,24 +5,27 @@ import FooterLink from './support/FooterLink';
 import FooterHelpers from './FooterHelpers';
 
 const FooterWrapper = styled.section`
-  padding: 1em;
-  background: #596e79;
+  border-radius: 50%;
 `;
 
 const FooterText = styled.h4`
-  padding: 2em 4em;
+  text-align: center;
+  margin-top: 0 auto;
+  font-size: 14px;
+  padding: 1em  0;
+  color: #596e79; 
 `;
 
 const Footer: React.FC = () => {
   return (
     <FooterWrapper role='footer'>
-      <FooterHelpers />
       <SocialMedia />
+      <FooterHelpers />
       <FooterText>
         Coded by
         <FooterLink href='https://www.lunasmithart.com' target='_blank'>Luna Smith</FooterLink>
         , open-source on
-        <FooterLink href='https://github.com/Lu-Smith/community-website-react' target='_blank'>GitHub</FooterLink>
+        <FooterLink href='https://github.com/Lu-Smith/community-website-react' target='_blank'>GitHub</FooterLink>.
       </FooterText>
     </FooterWrapper>
   )
