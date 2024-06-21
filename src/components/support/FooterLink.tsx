79744d484992819cf1a2e717a18a04c959caaca4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-21 21:10:06 +0100

    Updated FooterLink.tsx

--- src/components/support/FooterLink.tsx
+++ src/components/support/FooterLink.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterLinkElement = styled.a`
-  background: green;
-  color: white;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.4em 1.2em;
+  color: #596e79; 
+  padding: 0.4em 0.5em;
+  text-decoration: none;
+  transition: 200ms all ease;
 
   &:hover {
-    background: black;
+    color: black; 
   }
 `;
 
