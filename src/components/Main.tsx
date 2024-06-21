0911ca675dbab7efe309c333c3cc098965fb8f1c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-21 18:32:14 +0100

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,25 +3,36 @@ import styled from 'styled-components';
 import content from '../assets/content';
 
 const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
+  padding: 1em;
+  background: #dfd3c3;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const SectorComponent = styled.div`
+  background-color: #f0ece2;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+  padding: 25px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-`;
-
-const SectorComponent = styled.div`
-  background-color: grey;
+  gap: 30px;
+  height: 300px;
 `;
 
 const ContentContainer = styled.div`
-  background-color: grey;
-
+  align-text: left;
+  flex: 2;
 `;
 
 const Title = styled.h2`
   color: black;
+  margin-bottom: 10px;
 `;
 
 const Content = styled.p`
@@ -29,7 +40,9 @@ const Content = styled.p`
 `;
 
 const Image = styled.img`
-  width: 300px;
+  width: 200px;
+  height: auto;
+  flex: 1;
 `;
 
 
