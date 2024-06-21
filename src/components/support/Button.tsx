81ff23799a4d14eb382e888cbd3c7bbeb6338119
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-21 21:29:33 +0100

    Updated Button.tsx

--- src/components/support/Button.tsx
+++ src/components/support/Button.tsx
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 const ButtonElement = styled.button`
 	background: #596e79;
   color: #f0ece2;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.4em 1.2em;
+  font-size: 0.8em;
+  padding: 0.8em 1.2em;
+  margin: 0.5em;
   border: none;
   border-radius: 15px;
   box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.5);
