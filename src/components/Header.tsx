585c29dbad450c91d2dd0e0cc49dd654525d9b96
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-23 23:00:21 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import Button from './support/Button';
 import Community from '../assets/media/community.svg';
 import { motion } from "framer-motion";
+import { PiPersonArmsSpreadThin } from "react-icons/pi";
+import { PiPersonSimpleCircleLight } from "react-icons/pi";
+
 
 const HeaderWrapper = styled.section`
   padding: 0.2em 0.5em;
@@ -37,6 +40,11 @@ const Title = styled.h1`
   color: black; 
   text-align: left;
   font-family: "Playwrite US Modern", cursive;
+  letter-spacing: 1px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
 
 const IntroContainer = styled.div`
@@ -63,12 +71,30 @@ const Header: React.FC = () => {
   return (
     <HeaderWrapper role='header'>
         <Container className='first'>
-            <Title>Community Hub</Title>
-            <IntroContainer>
-                <IntroTitle>Build The Community Your Fans Will Love</IntroTitle>
-                <IntroContent>The Community Hub re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</IntroContent>
-            </IntroContainer>
-            <Button>Get started</Button>
+          <motion.div>
+            <Title>
+              C
+              <motion.div 
+              initial={{ scale: 1, y: 5 }}
+              whileHover={{ scale: 0.8, y: 20 }}
+              transition={{ duration: 0.6 }}>
+                <PiPersonSimpleCircleLight />
+            </motion.div>
+              mmun
+              <motion.div 
+              initial={{ scale: 1, y: 5 }}
+              whileHover={{ scale: 0.8, y: 20 }}
+              transition={{ duration: 0.6 }}>
+                <PiPersonArmsSpreadThin />
+              </motion.div>
+              ty Hub
+            </Title>
+          </motion.div>
+          <IntroContainer>
+              <IntroTitle>Build The Community Your Fans Will Love</IntroTitle>
+              <IntroContent>The Community Hub re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</IntroContent>
+          </IntroContainer>
+          <Button>Get started</Button>
         </Container>
         <Container className='second'>
             <Button>Try it For Free</Button>
