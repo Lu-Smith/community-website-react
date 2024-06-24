bbe8c45d21f05fac190e408d01bcd36cfcf02875
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-24 22:16:35 +0100

    Updated Main.test.tsx - removed React

--- src/components/Main.test.tsx
+++ src/components/Main.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import React from 'react';
 import { describe, it, expect, vi } from 'vitest';
 import Main from './Main';
 
