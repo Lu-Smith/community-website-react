a59baf1c32160606e98ca3579c504d8f7b7e21c5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-25 19:52:28 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import styled from 'styled-components';
 import { motion } from "framer-motion";
+import Button from "./components/support/Button";
 
 const AppWrapper = styled.div`
   background: #dfd3c3;
   overflow-x: hidden;
+  text-align: center;
 `;
 
 const App = () => {
+
+  const handleScrollToTop = () => {
+    const topElement = document.getElementById('top');
+    if (topElement) {
+      topElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <AppWrapper>
@@ -18,14 +27,22 @@ const App = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 2 }}
       >
-        <Header />
+        <Header id='top' />
       </motion.div>
       <div><Main /></div>
+      <div>
+        <Button onClick={handleScrollToTop}>
+          Scroll
+        </Button>
+      </div>
       <motion.div
         initial={{ opacity: 0}}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 2 }}
-      ><Footer /></motion.div>
+      >
+        <Footer />
+      </motion.div>
+     
     </AppWrapper>
   )
 }
